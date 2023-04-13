b4bed9adbeaee59d14bed336e95640ed23dc916b
SantiagoC09 <santiago31800@hotmail.com>
2023-04-13 13:23:14 -0500

    Implementacion menu lateral home v1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,27 @@
 import React from "react";
+import menuLateralImg from '../components/Images/menu_lateral.png';
 import './Home.css';
 
-const Home = () => {
 
+const Home = () => {
     return(
         <div className="home-container">
             <div className="titulo">
                 <h1>JuanMCell</h1>
             </div>
+            <div className="icono-menu-lateral">
+                <img src={menuLateralImg} alt="Icono del menú lateral" />
+            </div>
+            <div className="contenido_menu_lateral" id="menu_lateral">
+                <ul>
+                    <li>Productos</li>
+                    <li>Ventas</li>
+                    <li>Registros</li>
+                </ul>
+            </div>
         </div>
     )
 };
+
 
 export default Home;
