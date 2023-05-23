05c9ce6fadb379d4c97f608e89cf63837ac0bf59
SantiagoCC09 <santiago31800@hotmail.com>
2023-05-23 12:12:41 -0500

    Contabilidad y nuevo diseño

--- src/pages/Contabilidad/Contabilidad.js
+++ src/pages/Contabilidad/Contabilidad.js
@@ -3,16 +3,98 @@ import './Contabilidad.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from '../components/Navigation/Navigation';
 
+const Contabilidad = () => {
+    const [showTable1, setShowTable1] = useState(false);
+    const [showTable2, setShowTable2] = useState(false);
 
+    const handleShowTable1 = () => {
+        setShowTable1(true);
+        setShowTable2(false);
+    };
 
-class Contabilidad extends React.Component {
-    render(){
-        return(
-            <Navigation>
-                
-            </Navigation>
-        )
-    }
-}
+    const handleShowTable2 = () => {
+        setShowTable1(false);
+        setShowTable2(true);
+    };
+
+    const data = []; // Aquí debes proporcionar los datos para la tabla
+
+    return (
+        <Navigation>
+            <div className='button-alternar'>
+                <div className='button-alternar-display'>
+                    <button onClick={handleShowTable1}>Mostrar Tabla Display</button>
+                </div>
+
+                <div className='button-alternar-articulo'>
+                    <button onClick={handleShowTable2}>Mostrar Tabla Articulo</button>
+                </div>
+
+            </div>
+
+            {showTable1 && (
+                <div className='tabla-display'>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Marca</th>
+                                <th>Referencia</th>
+                                <th>Tipo</th>
+                                <th>Cantidad</th>
+                                <th>Costo Proveedor</th>
+                                <th>Precio</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((elemento) => (
+                                <tr key={elemento.id}>
+                                    <td>{elemento.id}</td>
+                                    <td>{elemento.marca}</td>
+                                    <td>{elemento.referencia}</td>
+                                    <td>{elemento.tipo}</td>
+                                    <td>{elemento.cantidad}</td>
+                                    <td>{elemento.costoProveedor}</td>
+                                    <td>{elemento.precio}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+
+            {showTable2 && (
+                <div className='tabla-articulo'>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Categoria</th>
+                                <th>Marca</th>
+                                <th>Cantidad</th>
+                                <th>Precio</th>
+                                <th>Costo Proveedor</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((elemento) => (
+                                <tr key={elemento.id}>
+                                    <td>{elemento.id}</td>
+                                    <td>{elemento.categoria}</td>
+                                    <td>{elemento.marca}</td>
+                                    <td>{elemento.cantidad}</td>
+                                    <td>{elemento.precio}</td>
+                                    <td>{elemento.costoProveedor}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+        </Navigation>
+    );
+};
 
 export default Contabilidad;
--- src/pages/components/Navigation/Navigation.jsx
+++ src/pages/components/Navigation/Navigation.jsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navigation.css';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-import { Link, useLocation} from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navigation = (props) => {
   const location = useLocation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleLogout = () => {
+    // Aquí puedes realizar cualquier lógica adicional antes de cerrar sesión
+  };
+
   return (
     <div>
-      <Navbar color='dark'>
+      <Navbar className='navbar'>
         <NavbarBrand href='/home'>
-
           <img src={process.env.PUBLIC_URL + '/favicon.ico'} width="50" height="50" alt="Logo" />
           <span className={location.pathname === '/home' ? 'nav-link active' : 'nav-link inicio'}>Inicio</span>
         </NavbarBrand>
         <Nav className="mr-auto">
           <NavItem>
-          <NavLink tag={Link} to='/display' className={location.pathname === '/display' ? 'active' : ''}>
-            Display
-          </NavLink>
-          </NavItem>
-          <NavItem>
-          <NavLink tag={Link} to='/articulo' className={location.pathname === '/articulo' ? 'active' : ''}>
-            Articulo
-          </NavLink>
-          </NavItem>
-          <NavItem>
-          <NavLink tag={Link} to='/contabilidad' className={location.pathname === '/contabilidad' ? 'active' : ''}>
-            Contabilidad
-          </NavLink>
+            <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
+              <DropdownToggle caret>
+                Cuenta
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>
+                  <Link to="/">Cerrar sesión</Link>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
           </NavItem>
         </Nav>
       </Navbar>
@@ -36,5 +42,6 @@ const Navigation = (props) => {
       </div>
     </div>
   );
-}
+};
+
 export default Navigation;
