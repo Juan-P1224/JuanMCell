a9af5a8a821107426705bbee44219ffcbe81f89b
SantiagoCC09 <santiago31800@hotmail.com>
2023-05-23 17:20:02 -0500

    Reestructura contabilidad

--- src/pages/Contabilidad/Contabilidad.js
+++ src/pages/Contabilidad/Contabilidad.js
@@ -1,100 +1,155 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Contabilidad.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from '../components/Navigation/Navigation';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup } from 'reactstrap';
+import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter } from 'reactstrap';
 
-const Contabilidad = () => {
-    const [showTable1, setShowTable1] = useState(false);
-    const [showTable2, setShowTable2] = useState(false);
 
-    const handleShowTable1 = () => {
-        setShowTable1(true);
-        setShowTable2(false);
+class Contabilidad extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showTable1: false,
+            showTable2: false,
+            data: [],
+            modalVender: false,
+            form: {
+                id: '',
+                marca: '',
+                referencia: '',
+                tipo: '',
+                cantidad: '',
+                precio: '',
+                costoProveedor: ''
+            }
+        };
+    }
+
+    handleShowTable1 = () => {
+        this.setState({
+            showTable1: true,
+            showTable2: false
+        });
     };
 
-    const handleShowTable2 = () => {
-        setShowTable1(false);
-        setShowTable2(true);
+    handleShowTable2 = () => {
+        this.setState({
+            showTable1: false,
+            showTable2: true
+        });
     };
 
-    const data = []; // Aquí debes proporcionar los datos para la tabla
+    mostrarModalVender = (registro) => {
+        this.setState({ modalVender: true, form: registro });
+    };
 
-    return (
-        <Navigation>
-            <div className='button-alternar'>
-                <div className='button-alternar-display'>
-                    <button onClick={handleShowTable1}>Mostrar Tabla Display</button>
+    ocultarModalVender = () => {
+        this.setState({ modalVender: false });
+    };
+
+    vender = (form) => {
+    };
+
+    render() {
+        const { showTable1, showTable2, data, modalVender, form } = this.state;
+
+        return (
+            <Navigation>
+                <div className='button-alternar'>
+                    <div className='button-alternar-display'>
+                        <button name='buttonDisplay' onClick={this.handleShowTable1}>Mostrar Tabla Display</button>
+                    </div>
+
+                    <div className='button-alternar-articulo'>
+                        <button name='buttonArticulo' onClick={this.handleShowTable2}>Mostrar Tabla Articulo</button>
+                    </div>
                 </div>
 
-                <div className='button-alternar-articulo'>
-                    <button onClick={handleShowTable2}>Mostrar Tabla Articulo</button>
-                </div>
-
-            </div>
-
-            {showTable1 && (
-                <div className='tabla-display'>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Marca</th>
-                                <th>Referencia</th>
-                                <th>Tipo</th>
-                                <th>Cantidad</th>
-                                <th>Costo Proveedor</th>
-                                <th>Precio</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((elemento) => (
-                                <tr key={elemento.id}>
-                                    <td>{elemento.id}</td>
-                                    <td>{elemento.marca}</td>
-                                    <td>{elemento.referencia}</td>
-                                    <td>{elemento.tipo}</td>
-                                    <td>{elemento.cantidad}</td>
-                                    <td>{elemento.costoProveedor}</td>
-                                    <td>{elemento.precio}</td>
+                {showTable1 && (
+                    <div className='tabla-display'>
+                        <table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Marca</th>
+                                    <th>Referencia</th>
+                                    <th>Tipo</th>
+                                    <th>Cantidad</th>
+                                    <th>Costo Proveedor</th>
+                                    <th>Precio</th>
+                                    <th>Acciones</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
+                            </thead>
+                            <tbody>
+                                {data.map((elemento) => (
+                                    <tr key={elemento.id}>
+                                        <td>{elemento.id}</td>
+                                        <td>{elemento.marca}</td>
+                                        <td>{elemento.referencia}</td>
+                                        <td>{elemento.tipo}</td>
+                                        <td>{elemento.cantidad}</td>
+                                        <td>{elemento.costoProveedor}</td>
+                                        <td>{elemento.precio}</td>
+                                        <td><Button color="none" className="btn-vender" onClick={() => this.mostrarModalVender(elemento)}>Vender</Button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
 
-            {showTable2 && (
-                <div className='tabla-articulo'>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Categoria</th>
-                                <th>Marca</th>
-                                <th>Cantidad</th>
-                                <th>Precio</th>
-                                <th>Costo Proveedor</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((elemento) => (
-                                <tr key={elemento.id}>
-                                    <td>{elemento.id}</td>
-                                    <td>{elemento.categoria}</td>
-                                    <td>{elemento.marca}</td>
-                                    <td>{elemento.cantidad}</td>
-                                    <td>{elemento.precio}</td>
-                                    <td>{elemento.costoProveedor}</td>
+                {showTable2 && (
+                    <div className='tabla-articulo'>
+                        <table className='table'>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Categoria</th>
+                                    <th>Marca</th>
+                                    <th>Cantidad</th>
+                                    <th>Precio</th>
+                                    <th>Costo Proveedor</th>
+                                    <th>Acciones</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-        </Navigation>
-    );
-};
-
+                            </thead>
+                            <tbody>
+                                {data.map((elemento) => (
+                                    <tr key={elemento.id}>
+                                        <td>{elemento.id}</td>
+                                        <td>{elemento.categoria}</td>
+                                        <td>{elemento.marca}</td>
+                                        <td>{elemento.cantidad}</td>
+                                        <td>{elemento.precio}</td>
+                                        <td>{elemento.costoProveedor}</td>
+                                        <td><Button color="none" className="btn-vender" onClick={() => this.mostrarModalVender(elemento)}>Vender</Button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+                <Modal isOpen={modalVender}>
+                    <ModalHeader>
+                        <div><h3>Vender</h3></div>
+                    </ModalHeader>
+                    <ModalBody>
+                        <FormGroup>
+                            <label>
+                                Desea vender: {form.id}?
+                            </label>
+                        </FormGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="none" className='btn-aceptar1' onClick={() => this.vender(form)}>Aceptar</Button>
+                        <Button color="none" className='btn-cancelar1' onClick={this.ocultarModalVender}>Cancelar</Button>
+                    </ModalFooter>
+                </Modal>
+            </Navigation>
+        );
+    }
+}
 export default Contabilidad;
+
+
